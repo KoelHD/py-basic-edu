c5761ce7d1c2f05b7a1609ecc9b791526781c7d7
eugene <dlydz@yandex.ru>
2022-10-20 03:16:47 +0300

    update base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,27 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    pass
+    weight = 0
+    fuel = 0
+    fuel_consumption = 5
+    started = False
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if not self.started:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise LowFuelError
+
+    def move(self, distance):
+        if (self.fuel_consumption * distance) <= self.fuel:
+            self.fuel -= (self.fuel_consumption * distance)
+        else:
+            raise NotEnoughFuel
