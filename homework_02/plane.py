fee9cbf684cabf579e10179fb9e9ca733f922513
eugene <dlydz@yandex.ru>
2022-10-23 17:26:12 +0300

    fix car.py and plane.py

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -6,11 +6,12 @@ from homework_02.exceptions import CargoOverload
 
 
 class Plane(Vehicle):
+    cargo = 0
+    max_cargo = 100
+
     def __init__(self, weight, fuel, fuel_consumption, max_cargo):
         super().__init__(weight, fuel, fuel_consumption)
         self.max_cargo = max_cargo
-    cargo = 0
-    max_cargo = 100
 
     def load_cargo(self, load):
         if load + self.cargo <= self.max_cargo:
