957780cedb15d52f13855ea1f0c9289954f4335e
eugene <dlydz@yandex.ru>
2023-01-31 03:42:35 +0300

    add base.html

--- homework_05/app.py
+++ homework_05/app.py
@@ -10,3 +10,31 @@
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import render_template
+from werkzeug.exceptions import NotFound
+
+
+app = Flask(__name__)
+# app.register_blueprint()
+
+
+# @app.get("/")
+# def get_root():
+#     return render_template("index.html")
+
+
+@app.get("/index/")
+def get_index():
+    return 'test'
+
+
+@app.get('/about/')
+def get_about():
+    return 'about'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
